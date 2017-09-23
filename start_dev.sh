4c08d710cab09956f27260e13cc5bb1579ad5814
Yuan Gao <gaoyuankidult@gmail.com>
2017-09-24 00:06:06 +0200

    Added script to start development.

---
+++ start_dev.sh
@@ -0,0 +1,7 @@
+#/home/alex/Downloads/pycharm-community-2017.1.3/bin/pycharm.sh > /dev/null & disown
+source activate red-coast
+cd /home/alex/Desktop/red-coast
+/opt/Aldebaran/Choregraphe\ Suite\ 2.5/bin/choregraphe_launcher > /dev/null & disown
+
+
+
